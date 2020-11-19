e0094d82b28b3ba2b288674f9bd46e34b98529c4
Kenchir <kchirchir.dev@gmail.com>
2020-11-20 02:04:41 +0300

    Fixed checking of all request dict keys and error exception

--- app.py
+++ app.py
@@ -10,22 +10,36 @@ import pandas as pd
 @app.route('/predict_loan_eligibility', methods=['POST'])
 def predictLoanEligibility():
     data=request.get_json(force=True)
+    KeysExpected = ["ApplicantIncome", "CoapplicantIncome",
+                    "LoanAmount", "Credit_History", "Gender", "Married", "Dependents", "Education", "Property_Area"]
+ 
+    #Check to ensure all the fields are available from the input
+    for key in KeysExpected:
+        if key not in data:
+            return jsonify({'success': False, "Message": key+" field is missing"})
+ 
     #convert data to dataframe
     df=pd.DataFrame.from_dict(data)
-    loaded_model = pickle.load(open("./Model/randomForest.sav", 'rb'))
-    #Get the first row of dataframe
-    parameters = df.iloc[0]
-    #Predict if eligible or not
-    prediction = loaded_model.predict([parameters])
-    #Get the score out of 1
-    score= loaded_model.predict_proba([parameters])
+
+    try:
+        loaded_model = pickle.load(open("./Model/randomForest.sav", 'rb'))
+        #Get the first row of dataframe
+        parameters = df.iloc[0]
+        #Predict if eligible or not
+        prediction = loaded_model.predict([parameters])
+        #Get the score out of 1
+        score= loaded_model.predict_proba([parameters])
+        
+        eligible=False
+        if(prediction[0] == 1):
+            eligible=True
+
+        return jsonify({'success':True,'eligible': eligible, 'creditScore': score[0][1]})
     
-    eligible=False
-    if(prediction[0] == 1):
-        eligible=True
+    except ValueError:
+        return jsonify({'success': False, 'Message': "Invalid field data type"})
 
-    return jsonify({'eligible': eligible, 'creditScore': score[0][1]})
-
+    
 @app.errorhandler(404)
 def page_not_found(error):
     return jsonify({'success': False,'Message':'Not found'})
